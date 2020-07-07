8aad484a0b75a0fbcded4426bab07d8eecb5e8a9
Svitlana Zaster <szaster@protonmail.com>
2020-07-07 18:07:46 -0500

    Added runSearch function

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 //Dependencies
 
 const mysql = require("mysql2");
-const department = require("console.table");
-const role = require("console.table");
-const employee = require("console.table");
-const questionnaire = require("./sections/questionnaire");
-const fs = require("fs");
-// array of questions for user
-const questions = [];
+// const employees = require("console.table");
+// const questionnaire = require("./sections/questionnaire");
+
+// const role = require("console.table");
+// const employee = require("console.table");
+
+// const departments = [];
+// const roles = [];
+// const employees = [];
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -17,19 +19,49 @@ const connection = mysql.createConnection({
   database: "employees",
 });
 
-// function to write README file
-// function writeToFile(fileName, data) {
-//   fs.writeFile(fileName, data, (err) => {
+const data = {};
+
+// function queryRoles(connection) {
+//   connection.query("SELECT * FROM role", function (err, res) {
 //     if (err) throw err;
-//     process.exit(1);
-//     console.log("The file has been saved!");
+//     console.log(res);
+//     res.forEach((role) =>
+//       console.log(`${role.id}: ${role.title}: ${role.salary}`)
+//     );
+//     connection.end();
 //   });
 // }
+
+function processRoles(err, res) {
+  if (err) throw err;
+  res.forEach((role) => console.log(`${role.title}|${role.salary}`));
+  // data.roles = res;
+}
+function processEmployee(err, res) {
+  if (err) throw err;
+  res.forEach((employee) =>
+    console.log(
+      `${employee.person_id}|${employee.first_name}|${employee.last_name}|${employee.role_id}|${employee.manager_id}`
+    )
+  );
+}
+function processDepartment(err, res) {
+  if (err) throw err;
+  res.forEach((department) =>
+    console.log(`${department.Department_id}|${department.Name}`)
+  );
+}
 
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id" + connection.threadId);
-  afterConnection();
+  connection.query("SELECT * FROM role", processRoles);
+  connection.query("SELECT * FROM employee", processEmployee);
+  connection.query("SELECT * FROM department", processDepartment);
+  // questionnaire.welcomeScreen();
+  // init();
+  console.log(data);
+  connection.end();
 });
 
 function afterConnection() {
--- sections/questionnaire.js
+++ sections/questionnaire.js
@@ -13,8 +13,81 @@ function welcomeScreen() {
       type: "list",
       name: "welcome",
       message:
-        "Welcome! This application dynamically generates a professional README.md. Press continue to proceed?",
+        "Welcome! This application allows to view, add, update and edit departments, roles, employees. Press continue to proceed?",
       choices: ["Continue", "Exit"],
     },
   ]);
 }
+
+function handleExit(data) {
+  if(data.welcome === "Exit"){
+    process.exit(0);
+  }
+}
+
+const choices = {
+  viewAll: "View all Employees",
+  viewAllByDepartment: "View All Employees By Department",
+  vielAllByManager: "View All Employees By Manager",
+  addEmployee: "Add Employee",
+  removeEmployee: "Remove Employee",
+  updateEmployee: "Update Employee Role",
+  updateEmployeeManager:"Update Employee Manager"
+}
+
+function runSearch(){
+inquirer.
+    prompt({
+      type:"rawlist",
+      name:"prompts",
+      message: "What would you like to do?",
+      choices: [ Object.values(choices)
+            ],
+          })
+          .then(function(answer){
+            switch(answer.action) {
+              case choices.viewAll:
+                employeeSearch();
+                break;
+              
+              case choices.viewAllByDepartment:
+                employeesByDepartment();
+                break;
+
+              case choices.vielAllByManager:
+                employeesByManager();
+                break;
+
+              case choices.addEmployee:
+                addEmployee();
+                break;
+
+              case choices.removeEmployee:
+                removeEmployee();
+                break;
+
+              case choices.updateEmployee:
+                updateEmployee();
+                break;
+
+              case choices.updateEmployeeManager:
+                updateEmployeeManager();
+                break;
+            }
+          });
+}
+
+function 
+
+//function to initialize program
+// function init()
+//   questionnaire.welcomeScreen()
+//   .then(handleExit)
+//   .then(() => questionnaire.())
+//   // .then((upatedEmployees) => writeToEmployees(updatedEmployees))
+//   )
+
+
+
+
+module.exports = { welcomeScreen: welcomeScreen };
